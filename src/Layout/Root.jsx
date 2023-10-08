c1f09c69f588892cc56ee8a718327ae5f02a3931
Muddasir-Faiyaj <muddasirfaiyaj66@gmail.com>
2023-10-08 18:37:54 +0600

    complete footer section

--- src/Layout/Root.jsx
+++ src/Layout/Root.jsx
@@ -1,5 +1,6 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../Shared/Navbar/Navbar";
+import Footer from "../Components/Footer/Footer";
 
 const Root = () => {
   return (
@@ -12,6 +13,7 @@ const Root = () => {
       
         <Outlet></Outlet>
       </div>
+      <Footer></Footer>
     </div>
   );
 };
