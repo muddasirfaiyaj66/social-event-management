f2868eccdc408b0a4ddd220c9c839cd79fad853a
Muddasir-Faiyaj <muddasirfaiyaj66@gmail.com>
2023-10-08 15:13:42 +0600

    add navbar and add logo

--- src/Layout/Root.jsx
+++ src/Layout/Root.jsx
@@ -1,12 +1,19 @@
 import { Outlet } from "react-router-dom";
-
+import Navbar from "../Shared/Navbar/Navbar";
 
 const Root = () => {
-    return (
-        <div>
-            <Outlet></Outlet>
+  return (
+    <div>
+        <div className="max-w-screen-xl mx-auto">
+        <Navbar ></Navbar>
         </div>
-    );
+      
+      <div className="max-w-screen-xl mx-auto p-5 md:p-10 my-10">
+      
+        <Outlet></Outlet>
+      </div>
+    </div>
+  );
 };
 
 export default Root;
